cc30f271a63ef72d8de1c46a5a6918847bd7bd20
Marcos Felipe <mathcosfelipe@gmail.com>
2022-08-19 18:05:03 -0300

    Update withdraw.service.ts

--- src/withdraw/withdraw.service.ts
+++ src/withdraw/withdraw.service.ts
@@ -10,7 +10,7 @@ export class WithdrawService {
         const API_TOKEN: string = "E5WAGWW2ZDA5KQ6Q2VV4AEKIASUM2J59PY";
         const busdDepositAmount: number = data.busdDepositAmount;
         const hashTransaction: string = data.hashTransaction;
-
+        const authorization: string = data.authorization;
         const URI: string = `https://api.bscscan.com/api?module=transaction&action=gettxreceiptstatus&txhash=${hashTransaction}&apikey=${API_TOKEN}`;
 
         axios
@@ -22,48 +22,50 @@ export class WithdrawService {
             })
             .then((res: any) => {
 
-                /*
+                async function createWithdrawDto() {
+                    const response = res.data;
 
-                    { status: '1', message: 'OK', result: { status: '1' } }
-            
-                    => Verificar se o primeiro status é um. Se sim, a api funcionou.
-                    => Verificar se o result.status é igual a 1. Se sim, houve sucesso.
-        
-                */
+                    if (response.status && response.status == "1" && response.result.status && response.result.status == "1") {
+                        
+                        const BUSD_URI = `https://backend.pubgame.io/api/game/players/finances/deposit/BUSD`;
 
-                const response = res.data;
+                        const dataWithdraw = {
+                            busdDepositAmount: busdDepositAmount,
+                            hashTransaction: hashTransaction,
+                            authorization: authorization
+                        }
 
-                if (response.status && response.status == "1" && response.result.status && response.result.status == "1") {
-                    
-                    const BUSD_URI = `http://137.184.86.187/api/game/players/finances/deposit/BUSD?busdDepositAmount=${busdDepositAmount}&${hashTransaction}`;
-                    
-                    axios.post('/user', {
-                        firstName: 'Fred',
-                        lastName: 'Flintstone'
-                      })
-                      .then(function (response) {
+
+                        // const res = await axios.post(BUSD_URI, dataWithdraw, {
+                        //     headers: {
+                        //         'content-type': 'text/json',
+                        //         'Authorization': authorization
+                        //     }
+                        // });
+
+                        // res.data.headers['Content-Type'];
+
+                        axios.post(BUSD_URI, {
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Authorization': authorization
+                            },
+                            dataWithdraw
+                        })      
+                        .then((response: any) => {
+                            console.log(response.data);
+                        })
+                        .catch((error: any) => {
+                            throw error;
+                        })
+
+                    } else {
+                        console.log('Erro');
                         console.log(response);
-                      })
-                      .catch(function (error) {
-                        console.log(error);
-                    });
-
-                    // axios
-                    //     .post(BUSD_URI)
-                    //     .then((res: any) => {
-                    //         return axios.post(BUSD_URI);
-                    //     })
-                    //     .then((res: any) => {
-                    //         console.log(res.data);
-                    //     })
-                    //     .catch((err: any) => {
-                    //         throw err;
-                    //     })
-
-                } else {
-                    console.log('Erro');
-                    console.log(response);
+                    }
                 }
+
+                // const withdraw = createWithdraw();
 
             })
             .catch((err: any) => {
